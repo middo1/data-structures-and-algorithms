80051c91a133e11602cce0a5d483455d33868789
Middo1 <hamidadeshina1@gmail.com>
2025-11-16 19:01:56 +0100

    SECOND PUSH

--- numsub.py
+++ numsub.py
@@ -1,11 +1,11 @@
 def numSub(s):
-        sub_strings = s.split("0")
-        i = 0
-        for sub_string in sub_strings:
-            if len(sub_string) > 0:
-                    d = len(sub_string)
-                    while d > 0:
-                            i += d
-                            d -= 1
-        return i % (10 ** 9 + 7)
+    sub_strings = s.split("0")
+    i = 0
+    for sub_string in sub_strings:
+        if len(sub_string) > 0:
+                d = len(sub_string)
+                while d > 0:
+                        i += d
+                        d -= 1
+    return i % (10 ** 9 + 7)
         
